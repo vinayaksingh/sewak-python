a2fa10117f6eff3f55f4b3bc49b80921e6861ae1
vin <vin.sin.9987@gmail.com>
2019-09-28 12:02:51 +0530

    removing secret information

--- api/utils.py
+++ api/utils.py
@@ -64,9 +64,9 @@ def __spawn_twilio__(otp, phone_number):
 
     # TODO manage secret information.
     # Your Account SID from twilio.com/console
-    account_sid = "AC342847da381754ad0085aaffb40cf46a"
+    account_sid = ""
     # Your Auth Token from twilio.com/console
-    auth_token = "b6b91d29101d35f641a17bc7ae5413bc"
+    auth_token = ""
 
     client = Client(account_sid, auth_token)
 
